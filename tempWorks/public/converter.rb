4646d69bfde520e96b0ba2566ec1287e7f748b7a
root <root@localhost.localdomain>
2018-04-18 13:09:39 -0400

    add converter and description func

---
+++ tempWorks/public/converter.rb
@@ -0,0 +1,36 @@
+$LOAD_PATH << '.'
+require 'get_description.rb'
+
+def file_convert(srv_name)
+
+  current_dir = Dir.pwd + '/inspectorScandAll_reports'
+  unless Dir.exists?("inspectorScandAll_reports")
+    creat_dir = Dir.mkdir("#{current_dir}")
+  end 
+
+  puts "Dosya tipleri: txt, pdf, odt, md,xml"
+  puts "Lütfen istediğiniz dosya tipini yazınız: "
+  type = gets.chomp
+
+  description = Description.get_description(srv_name)
+
+  Dir.chdir("inspectorScandAll_reports") do
+    out = File.open("out_file.txt","w")
+    out.puts(description)
+    out.close
+    case type
+    when "txt"
+      system("pandoc out_file.txt -o report_file.txt")
+    when "pdf"
+      system("pandoc out_file.txt -o report_file.pdf")
+    when "odt"
+      system("pandoc out_file.txt -o report_file.odt")
+    when "md"
+      system("pandoc out_file.txt -o report_file.md")
+    when "xml"
+      system("pandoc out_file.txt -o report_file.xml")
+    end
+    File.delete("out_file.txt")
+  end
+
+end
--- tempWorks/public/get_description.rb
+++ tempWorks/public/get_description.rb
@@ -7,8 +7,9 @@ def get_description(service_name)
 
   page = Nokogiri::HTML(open(url))
 
+  puts page.css('div#TableWithRules td')[0].text
   puts page.css('div#TableWithRules td')[1].text
 
 end
 
-# => get_description("vsftpd 2.3.4") şeklinde çağırılır.
+get_description("vsftpd 2.3.4")
